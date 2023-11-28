fe4191c5bcf7cd1a8c34305be2b8e974713ce03b
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2023-11-29 01:10:29 +0500

    Slider Done Fixed All Points

--- src/components/ArticlesSlider.jsx
+++ src/components/ArticlesSlider.jsx
@@ -1,40 +1,74 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
 import Frame1 from "../assets/images/Frame1.svg"
-import PaginationPic from "../assets/images/Pagination.svg"
 import { Color, FontFamily } from '../style/consts/GlobalStyles'
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+import { useState, useRef } from 'react';
 
 const ArticlesSlider = () => {
+
+    const [entries, setEntries] = useState([
+        { title: 'How To work ?', text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy ' },
+        { title: 'How To work ?', text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy ' },
+    ]);
+
+    const [activeSlide, setActiveSlide] = useState(0);
+
+    const renderItem = ({ item }) => {
+        return (
+            <View style={{ marginLeft: 22, height: 420 }}>
+
+                <View style={styles.main}>
+                    <Text style={styles.frame}><Frame1 /></Text>
+                    <Text style={styles.heading}>{item.title}</Text>
+                    <Text style={styles.para}>{item.text} </Text>
+
+                </View>
+            </View>
+        );
+    };
+
+
     return (
         <View>
 
-            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                <View style={styles.container}>
-                    <View style={styles.main}>
-                        <Text style={styles.frame}><Frame1/></Text>
-                        <Text style={styles.heading}>How To work ?</Text>
-                        <Text style={styles.para}>Lorem Ipsum is simply dummy text
-                            of the printing and typesetting
-                            industry. Lorem Ipsum has been
-                            the industry's standard dummy 
-                        </Text>
-
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <SafeAreaView>
+                    <View>
+                        <Carousel
+                            layout="default"
+                            data={entries}
+                            renderItem={renderItem}
+                            sliderWidth={300}
+                            itemWidth={300}
+                            onSnapToItem={(index) => setActiveSlide(index)}
+                        />
+                        <Pagination
+                            dotsLength={6}
+                            activeDotIndex={activeSlide}
+                            containerStyle={{ paddingVertical: 20 }}
+                            dotStyle={{
+                                width: 12,
+                                height: 12,
+                                borderRadius: 5,
+                                marginHorizontal: 0,
+                                backgroundColor: 'rgba(241, 119, 32, 1)',
+                            }}
+                            inactiveDotStyle={{
+                                width: 12,
+                                height: 12,
+                                borderRadius: 5,
+                                marginHorizontal: 0,
+                                backgroundColor: 'rgba(227, 227, 227, 1)',
+                            }}
+                            inactiveDotOpacity={1}
+                            inactiveDotScale={1}
+                        />
                     </View>
-                    <View style={styles.main2}>
-                        <Text style={styles.frame}><Frame1/></Text>
-                        <Text style={styles.heading}>How To work ?</Text>
-                        <Text style={styles.para}>Lorem Ipsum is simply dummy text
-                            of the printing and typesetting
-                            industry. Lorem Ipsum has been
-                            the industry's standard dummy 
-                        </Text>
-
-                    </View>
-                </View>
+                </SafeAreaView>
             </ScrollView>
-            <Text style={styles.PaginationPic}><PaginationPic/></Text>
-            
         </View>
+
     )
 }
 
@@ -43,48 +77,48 @@ export default ArticlesSlider
 const styles = StyleSheet.create({
     container: {
         flexDirection: "row",
-        marginLeft:20
-        
+        marginLeft: 20
+
     },
     main: {
         width: 283,
         height: 407,
-        backgroundColor:"rgba(255, 211, 193, 1), rgba(254, 199, 179, 1)",
-        marginTop:92,
-        marginRight:30
+        backgroundColor: "rgba(255, 211, 193, 1), rgba(254, 199, 179, 1)",
+        marginTop: 92,
+        marginRight: 30
 
     },
     heading: {
         color: Color.darkBlue,
         fontSize: 16,
         fontFamily: FontFamily.poppinsSemiBold,
-        marginLeft:21
+        marginLeft: 21
     },
-    para:{
+    para: {
         color: Color.darkBlue,
-        fontSize:13,
+        fontSize: 13,
         fontFamily: FontFamily.poppinsRegular,
-        marginLeft:21,
-        width:"86%",
-        lineHeight:22,
+        marginLeft: 21,
+        width: "86%",
+        lineHeight: 22,
     },
-    frame:{
-        position:"relative",
-        top:-50,
-        left:33
+    frame: {
+        position: "relative",
+        top: -50,
+        left: 33
     },
     main2: {
         width: 283,
         height: 407,
-        backgroundColor:"rgba(251, 229, 231, 1), rgba(245, 203, 204, 1)",
-        marginTop:92,
-        marginRight:30
+        backgroundColor: "rgba(251, 229, 231, 1), rgba(245, 203, 204, 1)",
+        marginTop: 92,
+        marginRight: 30
 
     },
-    PaginationPic:{
-        marginTop:37,
-        margin:"auto",
-        alignSelf:"center",
-        marginBottom:55
+    PaginationPic: {
+        marginTop: 37,
+        margin: "auto",
+        alignSelf: "center",
+        marginBottom: 55
     }
 })
--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -1,84 +1,81 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { TouchableOpacity, View,Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/home/Home';
 import Articles from '../screens/articles/Articles';
 import Lightbulb from 'react-native-vector-icons/Foundation';
-import Setting1 from "react-native-vector-icons/AntDesign"
-import Dashboard from "react-native-vector-icons/MaterialCommunityIcons"
-import PieChart from "react-native-vector-icons/AntDesign"
-import Plus from "../assets/images/NavigationPlus.png"
-
-
-
+import Setting1 from 'react-native-vector-icons/AntDesign';
+import Dashboard from 'react-native-vector-icons/MaterialCommunityIcons';
+import PieChart from 'react-native-vector-icons/AntDesign';
+import Plus from '../assets/images/NavigationPlus.png';
 
 const Navigation = () => {
   const Tab = createBottomTabNavigator();
 
-  const CustomTabBarButton = ({children,onPress})=>{
-    <TouchableOpacity 
-      style={{
-        top:-50,
-        justifyContent:"center",
-        alignItems:"center"
-      }}
-    >
-      <View style={{
-        width:70
-      }}>{children}</View>
-    </TouchableOpacity>
-  }
+  const CustomTabBarButton = ({ children, onPress }) => {
+    return (
+      <TouchableOpacity
+        style={{
+          top: -50,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <View style={{
+          width: 70
+        }}>{children}</View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <Tab.Navigator screenOptions={
-      {
+    <Tab.Navigator
+      screenOptions={{
         headerShown: false,
         tabBarShowLabel: false,
-        tabBarStyle: { backgroundColor: '#rgba(255, 247, 239, 1)',height:70, shadowOffset: -10, shadowColor:"rgba(92, 108, 156, 0.10)" },
+        tabBarStyle: { backgroundColor: '#rgba(255, 247, 239, 1)', height: 70, shadowOffset: -10, shadowColor: 'rgba(92, 108, 156, 0.10)' },
         tabBarActiveTintColor: '#000000',
-      }
-    }>
+      }}
+    >
       <Tab.Screen name="Home" component={Home} options={() => ({
-        tabBarIcon: ({ focused}) => {
+        tabBarIcon: ({ focused }) => {
           const lightbulb = focused ? 'rgba(251, 124, 0, 1)' : 'rgba(251, 124, 0, 0.52)';
           return <Dashboard name="view-dashboard" size={30} color={lightbulb} />;
         },
       })} />
-      <Tab.Screen name="Articles" component={Articles}  options={() => ({
-        tabBarIcon: ({ focused}) => {
+      <Tab.Screen name="Articles" component={Articles} options={() => ({
+        tabBarIcon: ({ focused }) => {
           const lightbulb = focused ? 'rgba(251, 124, 0, 1)' : 'rgba(251, 124, 0, 0.52)';
           return <Lightbulb name="lightbulb" size={30} color={lightbulb} />;
         },
       })} />
-      <Tab.Screen name="Articles2" component={Articles}  options={() => ({
-        tabBarIcon: ({ focused}) => (
-            <Image
+      <Tab.Screen name="Articles2" component={Articles} options={() => ({
+        tabBarIcon: ({ focused }) => (
+          <Image
             source={Plus}
             resizeMode='contain'
             style={{
-              top:-33,
-              backgroundColor:"transparent",
-              borderRadius:0
+              top: -33,
+              backgroundColor: 'transparent',
+              borderRadius: 0
             }}
-            />
+          />
         ),
-  
       })} />
       <Tab.Screen name="Home1" component={Home} options={() => ({
-        tabBarIcon: ({ focused}) => {
+        tabBarIcon: ({ focused }) => {
           const lightbulb = focused ? 'rgba(251, 124, 0, 1)' : 'rgba(251, 124, 0, 0.52)';
           return <PieChart name="piechart" size={30} color={lightbulb} />;
         },
-      })}  />
+      })} />
       <Tab.Screen name="Articles1" component={Articles} options={() => ({
-        tabBarIcon: ({ focused}) => {
+        tabBarIcon: ({ focused }) => {
           const lightbulb = focused ? 'rgba(251, 124, 0, 1)' : 'rgba(251, 124, 0, 0.52)';
           return <Setting1 name="setting" size={30} color={lightbulb} />;
         },
       })} />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
-export default Navigation
-
-const styles = StyleSheet.create({})
+export default Navigation;
